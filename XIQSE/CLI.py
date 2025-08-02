04611252d92e746fb4669fa6a72434a81bbc3420
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-02 22:43:21 +0200

    Update class

--- XIQSE/CLI.py
+++ XIQSE/CLI.py
@@ -1,5 +1,9 @@
-def test():
-    print("Ceci est un test depuis XIQSE.CLI !")
+class CLI(object):
+    def __init__(self, context):
+        self.ctx = context
 
-def send(cmd, returnCliError=False, msgOnError=None, WaitForPrompt=True):
-    emc_cli.send(cmd, waitFormPrompt)
+    def send(cmd, returnCliError=False, msgOnError=None, WaitForPrompt=True):
+        self.ctx.emc_cli.send(cmd, waitFormPrompt)
+        
+    def test():
+        print("Ceci est un test depuis XIQSE.CLI !")
--- XIQSE/__init__.py
+++ XIQSE/__init__.py
@@ -1,1 +1,7 @@
-import CLI
+from CLI import CLI
+
+class XIQSE(object):
+    def __init__(self, emc_cli=None):
+        self.emc_cli = emc_cli
+
+        self.CLI = CLI(self)
