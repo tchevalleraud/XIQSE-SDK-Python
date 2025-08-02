72f737708e9c895a4db50d4d54a1d17942295e02
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-02 21:34:59 +0200

    Update setup.py

---
+++ xiqse/setup.py
@@ -0,0 +1,20 @@
+"""
+    ExtremeCloudIQ Site Engine SDK
+"""
+
+from setuptools import setup, find_packages
+
+NAME = "xiqse"
+VERSION = "25.0.0-1"
+
+REQUIRES = [
+    "urllib3 ~= 1.26.7",
+]
+
+SETUP(
+    name=NAME,
+    version=VERSION,
+    description="ExtremeClouqIQ Site Engine",
+    author="Thibault CHEVALLERAUD",
+    author_email="tchevalleraud@extremenetworks.com",
+)
