60c275ee1320b2b8b0225e01c403713e3860fcbb
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-03 01:09:07 +0200

    Update CLI.py

--- XIQSE/CLI.py
+++ XIQSE/CLI.py
@@ -3,9 +3,29 @@ import time
 
 class CLI(object):
     RegexPrompt = re.compile('.*[\?\$%#>]\s?$')
+    RegexError  = re.compile(
+        '^%|\x07|error|invalid|cannot|unable|bad|not found|not exist|not allowed|no such|out of range|incomplete|failed|denied|can\'t|ambiguous|do not|unrecognized',
+        re.IGNORECASE | re.MULTILINE
+    )
+    RegexNoError  = re.compile(
+        '(?:'
+        + 'Both ends of MACsec link cannot have the same key-parity value'
+        + '|% Saving \d+ bytes to flash:startup-config'
+        + ')',
+        re.IGNORECASE | re.MULTILINE
+    )
 
     def __init__(self, context):
         self.ctx = context
+
+    def abortError(self, cmd, errorOutput):
+        print "Aborting script due to error on previous command"
+        try:
+            print "#TODO : rollbackStack"
+            #rollbackStack()
+        finally:
+            print "Aborting because this command failed: {}".format(cmd)
+            exitError(errorOutput)
     
     def cleanOutput(self, outputStr):
         if re.match(r'Error:', outputStr):
@@ -16,6 +36,12 @@ class CLI(object):
             return '\n'.join(outputLines[1:-1])
         else:
             return '\n'.join(outputLines[1:])
+    
+    def cliError(self, outputStr):
+        if not self.RegexNoError.search(outputStr) and self.RegexError.search(outputStr):
+            return True
+        else:
+            return False
     
     def exitError(self, errorOutput, sleep=10):
         if 'workflowMessage' in self.ctx.emc_vars:
@@ -31,6 +57,13 @@ class CLI(object):
         resultObj = self.ctx.emc_cli.send(cmd, waitForPrompt)
         if resultObj.isSuccess():
             outputStr = self.cleanOutput(resultObj.getOutput())
+            if outputStr and self.cliError("\n".join(outputStr.split("\n")[:4])):
+                if returnCliError:
+                    LastError = outputStr
+                    if msgOnError:
+                        print "==> Ignoring above error: {}\n\n".format(msgOnError)
+                    return None
+                abortError(cmd, outputStr)
             LastError = None
             return outputStr
         else:
