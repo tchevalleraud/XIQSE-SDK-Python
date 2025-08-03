e57160cac79197d9479fb96525e344fb603c65fc
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-03 04:50:10 +0200

    Update GraphQL

--- XIQSE/GraphQL.py
+++ XIQSE/GraphQL.py
@@ -24,7 +24,7 @@ class GraphQL(object):
         jsonQuery = replaceKwargs(jsonQueryDict['json'], kwargs)
         returnKey = jsonQueryDict['key'] if 'key' in jsonQueryDict else None
         self.ctx.debug("NBI Query:\n{}\n".format(jsonQuery))
-        response = nbiSessionPost(jsonQuery, returnKeyError) if self.NbiUrl else self.ctx.emc_nbi.query(jsonQuery)
+        response = self.nbiSessionPost(jsonQuery, returnKeyError) if self.NbiUrl else self.ctx.emc_nbi.query(jsonQuery)
         self.ctx.debug("nbiQuery response = {}".format(response))
         if response == None:
             return None
