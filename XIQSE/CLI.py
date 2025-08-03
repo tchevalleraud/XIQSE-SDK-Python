5a8aea4f1fa5433b0a2945b43e29feef5c808390
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-03 16:54:22 +0200

    Update CLI.py

--- XIQSE/CLI.py
+++ XIQSE/CLI.py
@@ -1,10 +1,11 @@
+import sys
+
 class CLI(object):
     def __init__(self, context):
         self.ctx = context
-    
+
     def sendCommand(self, cmd, returnCliError=False, msgOnError=None, waitForPrompt=True):
-        print self.ctx.emc_cli.__class__
-        resultObj = self.ctx.emc_cli.send(cmd, waitForPrompt)
+        resultObj = self.sendCommandSilent(cmd, returnCliError, msgOnError, waitForPrompt)
         if resultObj.isSuccess():
             resultLines = resultObj.getOutput().splitlines()[1:-1]
             for line in resultLines:
@@ -12,6 +13,18 @@ class CLI(object):
         else:
             print 'CLI-ERROR: ' + resultObj.getError()
             return False
+    
+    def sendCommandSilent(self, cmd, returnCliError=False, msgOnError=None, waitForPrompt=True):
+        original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+        try:
+            result = self.ctx.emc_cli.send(cmd, waitForPrompt)
+        finally:
+            sys.stdout.close()
+            sys.stdout = original_stdout
+        
+        return result
     
     def test(self):
         self.ctx.log("XIQSE.CLI.test => OK")
