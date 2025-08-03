0596c5e306123a87b59e39b5f7e1e5ad809f1ce7
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-03 15:20:24 +0200

    Update CLI Dict

---
+++ XIQSE/Dict/CLI.py
@@ -0,0 +1,12 @@
+CLI_Dict = {
+    'Fabric Engine': {
+        'config_context'        : 'config terminal',
+        'disable_more_paging'   : 'terminal more disable',
+        'enable_context'        : 'enable',
+        'end_config'            : 'end',
+        'exit_config_context'   : 'exit',
+        'port_config_context'   : 'interface gigabitEthernet {}',
+        'save_config'           : 'save config',
+        'vlan_config_context'   : 'interface vlan {}',
+    }
+}
