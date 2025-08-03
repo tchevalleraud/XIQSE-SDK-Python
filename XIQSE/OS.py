3e2ea8bb19d988a68056a2b1b4e0f80e65db1539
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-03 02:09:56 +0200

    update OS

--- XIQSE/OS.py
+++ XIQSE/OS.py
@@ -8,14 +8,13 @@ class OS(object):
     def __init__(self, context):
         self.ctx = context
     
-    def execute(self, cmd, debugOutput=True):
+    def execute(self, cmd, output=True):
         try:
-            output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-            if debugOutput:
-                return output
-            else:
-                return True
+            outputStr = subprocess.check_output(cmd)
         except Exception as e:
             print"{}: {}".format(type(e).__name__, str(e))
             print "Error executing '{}' on XIQSE shell".format(cmd)
             return False
+        
+        print("XIQSE.OS.execute : command {}".format(cmd))
+        print(outputStr)
