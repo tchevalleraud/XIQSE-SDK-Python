5b0546dfd64dfe4bab88a1364e4064167de71c88
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-03 02:07:52 +0200

    Update OS.py

--- XIQSE/OS.py
+++ XIQSE/OS.py
@@ -8,10 +8,10 @@ class OS(object):
     def __init__(self, context):
         self.ctx = context
     
-    def execute(self, cmd, output=True):
+    def execute(self, cmd, debugOutput=True):
         try:
             output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-            if output:
+            if debugOutput:
                 return output
             else:
                 return True
