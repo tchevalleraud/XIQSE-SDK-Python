73789fe6b3209712a80753d8bed0bda94ec96934
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-08-03 02:00:28 +0200

    Update OS.py

--- XIQSE/OS.py
+++ XIQSE/OS.py
@@ -1,4 +1,8 @@
+import re
+import subprocess
 import os
+
+from XIQSE.Utils import parseRegexInput
 
 class OS(object):
     def __init__(self, context):
@@ -12,3 +16,9 @@ class OS(object):
             print"{}: {}".format(type(e).__name__, str(e))
             print "Error executing '{}' on XIQSE shell".format(cmd)
             return False
+
+    def command(self, cmdRegexStr, debugKey=None):
+        mode, cmdList, regex = parseRegexInput(cmdRegexStr)
+        cmd = cmdList[0]
+        cmdList = cmd.split(' ')
+        debug("XIQSE.OS.command about to execute : {}".format(cmd))
---
+++ XIQSE/Utils/__init__.py
@@ -0,0 +1,14 @@
+import re
+
+def debug(debugOutput):
+    if Debug:
+        print debugOutput
+
+def parseRegexInput(cmdRegexStr):
+    if re.match(r'\w+(?:-\w+)?://', cmdRegexStr):
+        mode, cmdRegexStr = map(str.strip, cmdRegexStr.split('://', 1))
+    else:
+        mode = None
+    cmd, regex = map(str.strip, cmdRegexStr.split('||', 1))
+    cmdList = map(str.strip, cmd.split('&'))
+    return mode, cmdList, regex
