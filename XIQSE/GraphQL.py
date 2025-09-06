d9327ba2c39caed9b35a910f2b5d1e5038c49642
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-09-06 20:18:04 +0200

    FIX: GraphQL Function

--- XIQSE/GraphQL.py
+++ XIQSE/GraphQL.py
@@ -1,8 +1,15 @@
+import json
+import requests
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 from .Utils.NBIDict import NBI_Dict
+
 
 class GraphQL(object):
     def __init__(self, context):
         self.ctx = context
+        self.nbiUrl = None
     
     def nbiQuery(self, jsonQueryDict, debugKey=None, returnKeyError=False, **kwargs):
         global LastNbiError
@@ -10,10 +17,44 @@ class GraphQL(object):
         returnKey = jsonQueryDict['key'] if 'key' in jsonQueryDict else None
 
         self.ctx.debug("NBI Query:\n{}\n".format(jsonQuery))
+        
 
-    def nbiQueryDict(self, key):
-        self.ctx.log("NBI Key : {}".format(key))
-        print(NBI_Dict[key]['json'])
+    def nbiQueryDict(self, key, debugKey=None, returnKeyError=False, **kwargs):
+        return self.nbiQuery(NBI_Dict[key], debugKey, returnKeyError, **kwargs)
+
+    def nbiSessionPost(jsonQuery, returnKeyError=False):
+        global LastNbiError
+        session = request.Session()
+        session.verify = False
+        session.timeout = 10
+        session.auth = None
+        session.headers.update({
+            'Accept': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+        })
+        try:
+            response = session.post(self.nbiUrl, json={'operationName': None, 'query': jsonQuery, 'variables': None})
+            response.raise_for_status()
+        except requests.exceptions.RequestException as error:
+            if returnKeyError:
+                LastNbiError = error
+                return None
+            self.ctx.abortError("nbiQuery for \n{}".format(jsonQuery), error)
+        self.ctx.debug("nbiQuery response server = {}".format(response.headers['server']))
+        self.ctx.debug(f"nbiQuery response server version = {}".format(response.headers['server-version']))
+        try:
+            jsonResponse = json.loads(response.text)
+        except:
+            if returnKeyError:
+                LastNbiError = "JSON decoding failed"
+                return None
+            self.ctx.abortError("nbiQuery for\n{}".format(jsonQuery), "JSON decoding failed")
+        self.ctx.debug("nbiSessionPost() jsonResponse = {}".format(jsonResponse))
+        return jsonResponse
     
     def test(self):
         self.ctx.log("XIQSE.GraphQL.test => OK")
