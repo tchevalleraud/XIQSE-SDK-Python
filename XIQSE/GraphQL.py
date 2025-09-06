cf2a261493ee5de6d28d35d17a05aa544f6947d8
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-09-06 19:57:46 +0200

    FIX: GraphQL Function

--- XIQSE/GraphQL.py
+++ XIQSE/GraphQL.py
@@ -12,7 +12,8 @@ class GraphQL(object):
         self.ctx.debug("NBI Query:\n{}\n".format(jsonQuery))
 
     def nbiQueryDict(self, key):
-        self.ctx.log("NBI Query Dict: {}".format(NBI_Dict[key]))
+        self.ctx.debug("NBI Key : {}".format(key))
+        self.ctx.debug("NBI query : {}".format(NBI_Dict[key]['json']))
     
     def test(self):
         self.ctx.log("XIQSE.GraphQL.test => OK")
